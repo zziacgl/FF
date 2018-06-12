46f54bdc69dd522871f282d2c1da7a68984db563
陈高磊 <156788080@qq.com>
2018-06-12 15:10:02 +0800

    搜索改动

---
+++ duoduolicai/NewDeayou/ShellSections/Search/View/ShellSearchTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ShellSearchTableViewCell.h
+//  NewDeayou
+//
+//  Created by 陈高磊 on 2018/6/12.
+//  Copyright © 2018年 丰丰金融. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShellSearchTableViewCell : UITableViewCell
+
+@end
---
+++ duoduolicai/NewDeayou/ShellSections/Search/View/ShellSearchTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ShellSearchTableViewCell.m
+//  NewDeayou
+//
+//  Created by 陈高磊 on 2018/6/12.
+//  Copyright © 2018年 丰丰金融. All rights reserved.
+//
+
+#import "ShellSearchTableViewCell.h"
+
+@implementation ShellSearchTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
