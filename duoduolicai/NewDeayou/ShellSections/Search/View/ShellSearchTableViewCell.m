690de59a26f03e16bb71eb95a4a339476543952f
zhoubiwen <564648237@qq.com>
2018-06-13 15:36:25 +0800

    Revert "333"
    This reverts commit 5e546c4ee43920f94df698fffdb2a4418c66de9d.

--- duoduolicai/NewDeayou/ShellSections/Search/View/ShellSearchTableViewCell.m
+++ duoduolicai/NewDeayou/ShellSections/Search/View/ShellSearchTableViewCell.m
@@ -18,18 +18,7 @@
     self.searchBackView.layer.borderColor = kCOLOR_R_G_B_A(243, 181, 58, 1).CGColor;
     // Initialization code
 }
-- (void)setModel:(ShellRecordModel *)model {
-    if (model) {
-        self.nickLabel.text = [NSString stringWithFormat:@"昵称：%@", model.nickName];
-        NSArray *ary = model.goods;
-        NSLog(@"%@", ary);
-        ShellGoodsModel *goodsmodel = ary[0];
-        self.goodsLabel.text = [NSString stringWithFormat:@"商品:%@", goodsmodel.goodsName];
-        self.noteLabel.text = [NSString stringWithFormat:@"备注：%@", model.remark];
-        self.phoneLabel.text = [NSString stringWithFormat:@"手机号码:%@", model.mobile];
-    }
-    
-}
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
--- duoduolicai/NewDeayou/ShellSections/Search/controller/ShellSearchViewController.m
+++ duoduolicai/NewDeayou/ShellSections/Search/controller/ShellSearchViewController.m
@@ -8,14 +8,11 @@
 
 #import "ShellSearchViewController.h"
 #import "ShellSearchTableViewCell.h"
-#import "ShellModelTool.h"
-#import "AddRecordViewController.h"
 @interface ShellSearchViewController ()<UISearchBarDelegate,UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UISearchBar *searchBar;
 @property (nonatomic, assign) BOOL isContent;//判断搜索框是否有内容
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *dataArray;
-@property (nonatomic, strong) NSMutableArray *resultArray;
 @end
 static NSString *inentifier = @"ShellSearchTableViewCell";
 
@@ -26,9 +23,6 @@ static NSString *inentifier = @"ShellSearchTableViewCell";
     self.view.backgroundColor = [UIColor whiteColor];
     [self setOutNavView];
     [self setUpTableView];
-    [self.dataArray removeAllObjects];
-    [self.dataArray addObjectsFromArray:[ShellModelTool getAllRecord]];
-    NSLog(@"%@", self.dataArray);
     // Do any additional setup after loading the view.
 }
 - (void)viewDidAppear:(BOOL)animated
@@ -85,33 +79,23 @@ static NSString *inentifier = @"ShellSearchTableViewCell";
     [[UITableViewHeaderFooterView appearance] setTintColor:[UIColor clearColor]];
     
 }
-- (NSMutableArray *)dataArray {
-    if (!_dataArray) {
-        self.dataArray = [NSMutableArray array];
+
+- (void)searchText:(NSString *)text {
+    [self.dataArray removeAllObjects];
+    for (ShellRecordModel *model in [ShellModelTool getAllRecord]) {
+        if ([model.nickName containsString:text]) {
+            [self.dataArray addObject:model];
+        }
     }
-    return _dataArray;
+    [self.tableView reloadData];
+    
 }
-- (NSMutableArray *)resultArray {
-    if (!_resultArray) {
-        self.resultArray = [NSMutableArray array];
-    }
-    return _resultArray;
-}
-//- (void)searchText:(NSString *)text {
-//    [self.dataArray removeAllObjects];
-//    for (ShellRecordModel *model in [ShellModelTool getAllRecord]) {
-//        if ([model.nickName containsString:text]) {
-//            [self.dataArray addObject:model];
-//        }
-//    }
-//    [self.tableView reloadData];
-//
-//}
 
 #pragma mark -- UITableViewDelegate, UITableViewDataSource
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return _resultArray.count;
+//    return self.dataArray.count;
+    return 3;
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 108;
@@ -120,19 +104,10 @@ static NSString *inentifier = @"ShellSearchTableViewCell";
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     ShellSearchTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:inentifier forIndexPath:indexPath];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    
-    ShellRecordModel *model = self.resultArray[indexPath.row];
-    cell.model = model;
     return cell;
     
 }
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    AddRecordViewController *addVC = [[AddRecordViewController alloc] init];
-    addVC.hidesBottomBarWhenPushed = YES;
-    addVC.recordModel = self.resultArray[indexPath.row];
-    addVC.title = @"详情";
-    [self.navigationController pushViewController:addVC animated:YES];
-}
+
 #pragma mark - UISearchBarDelegate
 - (BOOL)searchBarShouldBeginEditing:(UISearchBar *)searchBar{
     return YES;
@@ -156,35 +131,6 @@ static NSString *inentifier = @"ShellSearchTableViewCell";
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
 {
 //    NSString *inputStr = searchText;
-    NSLog(@"输入%@", searchText);
-    [self.resultArray removeAllObjects];
-    dispatch_queue_t globalQueue = dispatch_get_global_queue(0, 0);
-    dispatch_async(globalQueue, ^{
-        if (searchText!=nil && searchText.length>0) {
-            
-            //遍历需要搜索的所有内容，其中self.dataArray为存放总数据的数组
-            for (ShellRecordModel *model in self.dataArray) {
-                
-                NSString *tempStr = model.nickName;
-                
-                //----------->把所有的搜索结果转成成拼音
-                NSString *pinyin = [self transformToPinyin:tempStr];
-                NSLog(@"pinyin--%@",pinyin);
-                
-                if ([pinyin rangeOfString:searchText options:NSCaseInsensitiveSearch].length >0 ) {
-                    //把搜索结果存放self.resultArray数组
-                    [self.resultArray addObject:model];
-                }
-            }
-        }
-        //回到主线程
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [_tableView reloadData];
-        });
-    });
-    
-    
-    
     
 }
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
@@ -192,52 +138,6 @@ static NSString *inentifier = @"ShellSearchTableViewCell";
     [self.view resignFirstResponder];
     
 }
-- (NSString *)transformToPinyin:(NSString *)aString
-{
-    //转成了可变字符串
-    NSMutableString *str = [NSMutableString stringWithString:aString];
-    CFStringTransform((CFMutableStringRef)str,NULL, kCFStringTransformMandarinLatin,NO);
-    //再转换为不带声调的拼音
-    CFStringTransform((CFMutableStringRef)str,NULL, kCFStringTransformStripDiacritics,NO);
-    NSArray *pinyinArray = [str componentsSeparatedByString:@" "];
-    NSMutableString *allString = [NSMutableString new];
-    
-    int count = 0;
-    
-    for (int  i = 0; i < pinyinArray.count; i++)
-    {
-        
-        for(int i = 0; i < pinyinArray.count;i++)
-        {
-            if (i == count) {
-                [allString appendString:@"#"];//区分第几个字母
-            }
-            [allString appendFormat:@"%@",pinyinArray[i]];
-            
-        }
-        [allString appendString:@","];
-        count ++;
-        
-    }
-    
-    NSMutableString *initialStr = [NSMutableString new];//拼音首字母
-    
-    for (NSString *s in pinyinArray)
-    {
-        if (s.length > 0)
-        {
-            
-            [initialStr appendString:  [s substringToIndex:1]];
-        }
-    }
-    
-    [allString appendFormat:@"#%@",initialStr];
-    [allString appendFormat:@",#%@",aString];
-    
-    return allString;
-}
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
